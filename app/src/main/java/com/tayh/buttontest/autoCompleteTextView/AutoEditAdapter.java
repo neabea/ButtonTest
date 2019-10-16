feac0b932cb619e95f1018ee6e7eff212e12b060
neabea <neabea2007@sina.com>
2019-10-16 11:31:00 +0800

    测试2

--- app/src/main/java/com/tayh/buttontest/autoCompleteTextView/AutoEditAdapter.java
+++ app/src/main/java/com/tayh/buttontest/autoCompleteTextView/AutoEditAdapter.java
@@ -43,6 +43,7 @@ public class AutoEditAdapter extends BaseAdapter implements Filterable {
 
     @Override
     public Filter getFilter() {
+
         if (mFilter == null) {
             mFilter = new ArrayFilter();
         }
